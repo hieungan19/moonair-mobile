b04269ca60d52076d486a6b797ae83cd38b81c84
thanhthu20 <thuthanhnguyen224@gmail.com>
2024-03-30 11:42:05 +0700

    tgt

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  //Base
+  static const Color primary = Color(0xFF064043);
+  static const Color secondary = Color(0xFF97C5B7);
+  static const Color grey1 = Color(0xFFECEDEE);
+  static const Color grey2 = Color(0xFF979797);
+  static const Color background = Color(0xFF0F1F7);
+
+  //Text
+  static const Color error = Color(0xFFEF5E5E);
+  static const Color info = Color(0xFF959595);
+  static const Color warning = Color(0xFFE0CA71);
+  static const Color blacktext = Color(0XFF212121);
+  static const Color whitetext = Color(0xFFFCFCFC);
+  static const Color onSurface = Color(0xFFFAFAFA);
+  static const Color onBg = Color(0xFF494949);
+  static const Color onSur = Color(0xFF494949);
+  static const Color onError = Color(0xFFFFFFFF);
+  static const Color onInfo = Color(0xFFD3D3D3);
+
+  //Tint
+  static const Color tint1 = Color(0xFF8AA5AD);
+  static const Color tint2 = Color.fromRGBO(202, 214, 218, 0.5);
+
+  // Drop shadow
+  static const Color dropShadow = Color.fromRGBO(4, 6, 15, 0.07);
+
+  static const Color gray400 = Color(0xFF999EA4);
+
+  static const Color text = Color(0xFF171827);
+}
