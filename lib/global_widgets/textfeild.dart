356ec2bed5aa66d4a8f67ac6cb8a2a801372175e
Khoile1110 <102748299+Kkomc1125@users.noreply.github.com>
2024-04-08 15:43:25 +0700

    Update textfeild.dart

--- lib/global_widgets/textfeild.dart
+++ lib/global_widgets/textfeild.dart
@@ -1,10 +1,1 @@
-import 'package:flutter/material.dart';
 
-class logo_big extends StatelessWidget {
-  const logo_big({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const TextField();
-  }
-}
